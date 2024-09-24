9e16f86ce5c72c51b63786c4dd24787f1570a000
SuperKiment <kiment2002@gmail.com>
2024-09-24 15:45:33 +0200

    Login & Profil

--- swipecamp/src/App.js
+++ swipecamp/src/App.js
@@ -3,7 +3,9 @@ import logo from "./img/Hippocampe.png";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./screens/HomeScreen";
 import Campus from "./screens/CampusScreen";
+import Login from "./screens/LoginScreen";
 import { UserProvider } from "./context/UserContext";
+import Profil from "./screens/ProfilScreen";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/campus" element={<Campus />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profil" element={<Profil />} />
         </Routes>
       </Router>
     </UserProvider>
--- swipecamp/src/screens/HomeScreen.js
+++ swipecamp/src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
 import DevTools from "../components/DevTools";
 import "../style/style.css";
 import logo from "../img/Hippocampe.png";
 import flyer from "../img/SwipeOCamp.png";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useId, useState, useEffect } from "react";
+import { bddURL } from "../config";
+import { useUserContext } from "../context/UserContext";
 
 function Home() {
   const navigate = useNavigate();
@@ -12,12 +13,13 @@ function Home() {
   const [campus, setCampus] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { user } = useUserContext();
 
   const getCampus = async () => {
     try {
-      const response = await fetch("https://s4-8096.nuage-peda.fr/campus");
+      const response = await fetch(bddURL + "campus");
       if (!response.ok) {
-        throw new Error('Erreur réseau');
+        throw new Error("Erreur réseau");
       }
       const data = await response.json();
       setCampus(data);
@@ -43,7 +45,23 @@ function Home() {
           </a>
         </div>
         <div className="navbar-menu">
-          <button>Mon espace</button>
+          {user ? (
+            <button
+              onClick={() => {
+                navigate("profil");
+              }}
+            >
+              {"Bonjour, " + user.firstName}
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                navigate("login");
+              }}
+            >
+              Mon Espace
+            </button>
+          )}
         </div>
       </nav>
       <div className="container">
