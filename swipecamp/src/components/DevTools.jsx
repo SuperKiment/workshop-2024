2b0927173724c52283c8fbf1ddbf7dd2d4a9e26f
SuperKiment <kiment2002@gmail.com>
2024-09-24 14:31:51 +0200

    link devtools

--- swipecamp/src/components/DevTools.jsx
+++ swipecamp/src/components/DevTools.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { useUserContext } from "../context/UserContext";
 import LoginForm from "./LoginForm";
+import { Link } from "react-router-dom";
 
 const DevTools = () => {
   const [visible, setVisible] = useState(true);
   const { user } = useUserContext();
 
   return (
-    visible && (
+    console.log(user) ||
+    (visible && (
       <div style={{ display: "flex", flexDirection: "row" }}>
         <p style={{ flex: 1 }}>DevTools</p>
         <a
@@ -28,12 +30,20 @@ const DevTools = () => {
         >
           {user
             ? "Connecté : " + user.firstName
-            : "Se connecter dev (test@test.com)"}
+            : "Non connecté"}
         </a>
 
         <LoginForm />
+
+        <div style={{ flex: 1 }}></div>
+        <div style={{ flex: 1 }}>
+            <ul>
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/campus"}>Campus</Link></li>
+            </ul>
+        </div>
       </div>
-    )
+    ))
   );
 };
 
--- swipecamp/src/screens/CampusScreen.js
+++ swipecamp/src/screens/CampusScreen.js
@@ -1,20 +1,23 @@
-import '../style/style.css';
-import logo from '../img/Hippocampe.png';
+import "../style/style.css";
+import logo from "../img/Hippocampe.png";
+import DevTools from "../components/DevTools";
 
 function Campus() {
   return (
     <div className="App back4">
+      <DevTools />
+
       <nav className="navbar">
-      <div className="navbar-logo">
-        <a href="/" >
-        <img src={logo} alt="Logo" className='logoImg'/>
-        <p>SWIPE O'CAMP</p>
-        </a>
-      </div>
-    </nav>
-    <div className='long'>
+        <div className="navbar-logo">
+          <a href="/">
+            <img src={logo} alt="Logo" className="logoImg" />
+            <p>SWIPE O'CAMP</p>
+          </a>
+        </div>
+      </nav>
+      <div className="long">
         <p>CHOIX DU CAMPUS</p>
-    </div>
+      </div>
     </div>
   );
 }
