94d2d785470a3855025d735f67d5a46a0311127c
Agathe2310 <poteaux.agathe@gmail.com>
2024-09-24 13:46:15 +0200

    campus

--- swipecamp/src/screens/HomeScreen.js
+++ swipecamp/src/screens/HomeScreen.js
@@ -2,11 +2,33 @@ import '../style/style.css';
 import logo from '../img/Hippocampe.png';
 import flyer from '../img/SwipeOCamp.png';
 import { useNavigate } from 'react-router-dom';
-import { useId } from 'react';
+import { useId, useState, useEffect } from 'react';
 
 function Home() {
   const navigate = useNavigate();
   const campusSelectId = useId();
+  const [campus, setCampus] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getCampus = async () => {
+    try {
+      const response = await fetch("https://s4-8096.nuage-peda.fr/campus");
+      if (!response.ok) {
+        throw new Error('Erreur réseau');
+      }
+      const data = await response.json();
+      setCampus(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCampus();
+  }, []);
 
   return (
     <div className="App back1">
@@ -23,16 +45,23 @@ function Home() {
       </nav>
       <div className='container'>
         <div className='twoColumns'>
-          <h1 >Découvre <em className='underline'>toute ​l’actualité</em> de ton ​campus !</h1>
-          {/* <button onClick={() => navigate('/campus')}>Je choisis mon campus</button> */}
+          <h1>Découvre <em className='underline'>toute ​l’actualité</em> de ton ​campus !</h1>
           <h2>Je choisis mon campus :</h2>
           <select id={campusSelectId} name="selectedCampus">
-            <option value="cucumber">Concombre</option>
-            <option value="corn">Maïs</option>
-            <option value="tomato">Tomate</option>
+            {loading ? (
+              <option>Chargement des campus...</option>
+            ) : error ? (
+              <option>{error}</option>
+            ) : (
+              campus.map((campusItem) => (
+                <option key={campusItem.idCampus} value={campusItem.name}>
+                  {campusItem.name}
+                </option>
+              ))
+            )}
           </select>
         </div>
-        <div className='twoColumns' >
+        <div className='twoColumns'>
           <img src={flyer} alt="flyer" className="flyer" />
         </div>
       </div>
