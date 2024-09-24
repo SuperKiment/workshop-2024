5169e9a2b1c2b3dac8e71704aeff27d9252362e8
SuperKiment <kiment2002@gmail.com>
2024-09-24 11:23:04 +0200

    Revert "Merge branch 'main' of github.com:SuperKiment/workshop-2024"
    This reverts commit 0d5ce84ca9a7c20789ab7ab1150954e05169675b, reversing
    changes made to 53eacb394c3234cf09b1451b5e341c36d1815080.

--- swipecamp/src/App.js
+++ swipecamp/src/App.js
@@ -1,21 +1,23 @@
-import './style/style.css';
-import logo from './img/Hippocampe.png';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="App back1">
-      <nav className="navbar">
-      <div className="navbar-logo">
-        <a>
-        <img src={logo} alt="Logo" className='logoImg'/>
-        <p>SWIPE O'CAMP</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-      </div>
-      <div className="navbar-menu">
-        <button>Mon espace</button>
-      </div>
-    </nav>
-    <div className='long'></div>
+      </header>
     </div>
   );
 }
