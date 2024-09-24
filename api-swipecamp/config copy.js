23193092011fd7b8ae49886a7dd7cc7b232fa323
SuperKiment <kiment2002@gmail.com>
2024-09-24 08:38:19 +0200

    Create config copy.js

---
+++ api-swipecamp/config copy.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+  host: "localhost", // Adresse du serveur MySQL
+  user: "", // Utilisateur nuage
+  password: "", // Mot de passe
+  database: "", // Remplace par le nom de ta base de données
+};
+
+export default dbConfig;
